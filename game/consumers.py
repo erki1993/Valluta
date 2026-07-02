39fd0a1cdba2f9df12affaa5e1d19e2dbde1384f
Erki Koplimets <erki.koplimets@gmail.com>
2026-07-02 20:50:11 +0300

    Add heartbeat logic

--- game/consumers.py
+++ game/consumers.py
@@ -1,7 +1,10 @@
+import asyncio
 import json
 
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+HEARTBEAT_INTERVAL = 1  # seconds
 
 
 class GameConsumer(AsyncWebsocketConsumer):
@@ -12,6 +15,7 @@ class GameConsumer(AsyncWebsocketConsumer):
         await self.accept()
         state = await database_sync_to_async(self._get_state)()
         await self.send(text_data=json.dumps(state))
+        self._heartbeat_task = asyncio.ensure_future(self._heartbeat_loop())
 
     def _get_state(self):
         from host.views import _get_display_game, _serialize_game_state
@@ -19,7 +23,16 @@ class GameConsumer(AsyncWebsocketConsumer):
         game = _get_display_game(game_id=self.game_id)
         return _serialize_game_state(game)
 
+    async def _heartbeat_loop(self):
+        try:
+            while True:
+                await asyncio.sleep(HEARTBEAT_INTERVAL)
+                await self.send(text_data=json.dumps({"heartbeat": True}))
+        except asyncio.CancelledError:
+            pass
+
     async def disconnect(self, close_code):
+        self._heartbeat_task.cancel()
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def receive(self, text_data):
